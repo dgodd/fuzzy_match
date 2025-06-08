303ff658ca545130e7734efee28318ec8d2ba1d4
Dave Goddard <dave@goddard.id.au>
2025-06-09 09:14:14 +1000

    add fts_fuzzy_match.h and convert from cpp to c

--- ext/fuzzy_match/fuzzy_match.c
+++ ext/fuzzy_match/fuzzy_match.c
@@ -1,4 +1,6 @@
 #include "fuzzy_match.h"
+#define FTS_FUZZY_MATCH_IMPLEMENTATION
+#include "fts_fuzzy_match.h"
 
 VALUE rb_mFuzzyMatch;
 VALUE rb_cFuzzyMatchExtension;
@@ -7,6 +9,11 @@ static VALUE
 rb_fuzzy_match_extension_class_do_something(VALUE self)
 {
   /* todo: perform CPU-intensive operation */
+  int outScore;
+  bool matched = fts_fuzzy_match_simple("got", "game of thrones", &outScore);
+  printf("Matched: %d\n", matched);
+  printf("Score: %d\n", outScore);
+
   return rb_str_new_cstr("something has been done");
 }
 
